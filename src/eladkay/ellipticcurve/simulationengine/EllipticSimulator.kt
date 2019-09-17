63acf5b51fee71533b4e584a5e6386abb30be0c4
Eladkay <eladkay@gmail.com>
2019-09-17 23:15:10 +0300

    added an offset (and i'll make it even more significant in the future) and removed that horrendous line

--- src/eladkay/ellipticcurve/simulationengine/EllipticSimulator.kt
+++ src/eladkay/ellipticcurve/simulationengine/EllipticSimulator.kt
@@ -5,18 +5,19 @@ import eladkay.ellipticcurve.mathengine.Vec2d
 import eladkay.ellipticcurve.mathengine.Vec2i
 
 object EllipticSimulator {
-    fun drawCurve(ellipticCurve: EllipticCurve, frame: CurveFrame, xScale: Int = 160, yScale: Int = 40) {
+    val X_OFFSET = -200
+    fun drawCurve(ellipticCurve: EllipticCurve, frame: CurveFrame, xScale: Int = 180, yScale: Int = 40) {
         ellipticCurve.field {
             var last: Vec2i? = null
             for(x in 0..frame.frameSize().x)
                 for(y in 0..frame.frameSize().y) {
-                    val xModified = (x - frame.frameSize().x/2)/xScale.toDouble()
+                    val xModified = (x - frame.frameSize().x/2 - X_OFFSET)/xScale.toDouble()
                     val yModified = (-y + frame.frameSize().y/2)/yScale.toDouble()
                     if(ellipticCurve.isPointOnCurve(Vec2d(xModified, yModified))) {
                         frame.drawPoint(Vec2i(x, y))
-                        frame.drawText(Vec2i(x, y), "($xModified, $yModified)")
-                        if(last != null) frame.drawLine(Vec2i(x, y), last)
-                        last = Vec2i(x, y)
+                        frame.drawText(Vec2i(x, y), "(${xModified}, $yModified)")
+                        /*if(last != null) frame.drawLine(Vec2i(x, y), last)
+                        last = Vec2i(x, y)*/
                     }
                 }
         }
@@ -24,7 +25,7 @@ object EllipticSimulator {
 
     fun drawAxis(frame: CurveFrame) {
         frame.drawLine(Vec2i(0, frame.frameSize().y/2), Vec2i(frame.frameSize().x, frame.frameSize().y/2))
-        frame.drawLine(Vec2i(frame.frameSize().x/2, 0), Vec2i(frame.frameSize().x/2, frame.frameSize().y))
+        frame.drawLine(Vec2i(frame.frameSize().x/2+X_OFFSET, 0), Vec2i(frame.frameSize().x/2+X_OFFSET, frame.frameSize().y))
     }
 
 }
